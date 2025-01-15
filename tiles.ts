e9759f13f1dcd239c33d147cc76186dff5a73ead
dcgs-sbu <sbu@challoners.com>
2025-01-15 21:56:22 +0000

    Create tiles.ts

---
+++ tiles.ts
@@ -0,0 +1,5 @@
+namespace customTiles_Additional {
+    //% fixedInstance jres blockIdentity=images._tile
+    //% tags="tile category-Additional"
+    export const tile1 = image.ofBuffer(hex``);
+}
