2cd122ceed2388c43ac3b9cec3aa5e535d6f156c
dcgs-sbu <sbu@challoners.com>
2025-03-07 07:31:56 +0000

    Update tiles.ts

--- tiles.ts
+++ tiles.ts
@@ -2,36 +2,51 @@ namespace customTiles_Additional {
     //% fixedInstance jres blockIdentity=images._tile
     //% tags="tile category-Additional"
     export const doorBlue = image.ofBuffer(hex``);
+    
     //% fixedInstance jres blockIdentity=images._tile
     //% tags="tile category-Additional"
     export const doorRed = image.ofBuffer(hex``);
+    
     //% fixedInstance jres blockIdentity=images._tile
     //% tags="tile category-Additional"
     export const doorGreen = image.ofBuffer(hex``);
+    
     //% fixedInstance jres blockIdentity=images._tile
     //% tags="tile category-Additional"
     export const keyBlue = image.ofBuffer(hex``);
+    
     //% fixedInstance jres blockIdentity=images._tile
     //% tags="tile category-Additional"
     export const keyRed = image.ofBuffer(hex``);
+    
     //% fixedInstance jres blockIdentity=images._tile
     //% tags="tile category-Additional"
     export const keyGreen = image.ofBuffer(hex``);
+    
     //% fixedInstance jres blockIdentity=images._tile
     //% tags="tile category-Additional"
     export const spikesUp = image.ofBuffer(hex``);
+    
     //% fixedInstance jres blockIdentity=images._tile
     //% tags="tile category-Additional"
     export const spikesDown = image.ofBuffer(hex``);
+    
     //% fixedInstance jres blockIdentity=images._tile
     //% tags="tile category-Additional"
     export const ladder = image.ofBuffer(hex``);
+
+    //% fixedInstance jres blockIdentity=images._tile
+    //% tags="tile category-Additional"
+    export const signWooden = image.ofBuffer(hex``);
+    
     //% fixedInstance jres blockIdentity=images._tile
     //% tags="tile category-Additional"
     export const ice = image.ofBuffer(hex``);
+    
     //% fixedInstance jres blockIdentity=images._tile
     //% tags="tile category-Additional"
     export const blockMetal = image.ofBuffer(hex``);
+    
     //% fixedInstance jres blockIdentity=images._tile
     //% tags="tile category-Additional"
     export const swordPickup = image.ofBuffer(hex``);
