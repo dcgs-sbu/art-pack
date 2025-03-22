f00a4dc120dac34095ca43afc4a258f76cec54ba
dcgs-sbu <sbu@challoners.com>
2025-03-22 05:51:00 +0000

    Update tiles.ts

--- tiles.ts
+++ tiles.ts
@@ -51,3 +51,69 @@ namespace customTiles_Additional {
     //% tags="tile category-Additional"
     export const swordPickup = image.ofBuffer(hex``);
 }
+
+namespace customTiles_Placeholders {
+    //% fixedInstance jres blockIdentity=images._tile
+    //% tags="tile category-Placeholders"
+    export const placeholder0 = image.ofBuffer(hex``);
+
+    //% fixedInstance jres blockIdentity=images._tile
+    //% tags="tile category-Placeholders"
+    export const placeholder1 = image.ofBuffer(hex``);
+    
+    //% fixedInstance jres blockIdentity=images._tile
+    //% tags="tile category-Placeholders"
+    export const placeholder2 = image.ofBuffer(hex``);
+    
+    //% fixedInstance jres blockIdentity=images._tile
+    //% tags="tile category-Placeholders"
+    export const placeholder3 = image.ofBuffer(hex``);
+    
+    //% fixedInstance jres blockIdentity=images._tile
+    //% tags="tile category-Placeholders"
+    export const placeholder4 = image.ofBuffer(hex``);
+    
+    //% fixedInstance jres blockIdentity=images._tile
+    //% tags="tile category-Placeholders"
+    export const placeholder5 = image.ofBuffer(hex``);
+    
+    //% fixedInstance jres blockIdentity=images._tile
+    //% tags="tile category-Placeholders"
+    export const placeholder6 = image.ofBuffer(hex``);
+    
+    //% fixedInstance jres blockIdentity=images._tile
+    //% tags="tile category-Placeholders"
+    export const placeholder7 = image.ofBuffer(hex``);
+    
+    //% fixedInstance jres blockIdentity=images._tile
+    //% tags="tile category-Placeholders"
+    export const placeholder8 = image.ofBuffer(hex``);
+    
+    //% fixedInstance jres blockIdentity=images._tile
+    //% tags="tile category-Placeholders"
+    export const placeholder9 = image.ofBuffer(hex``);
+    
+    //% fixedInstance jres blockIdentity=images._tile
+    //% tags="tile category-Placeholders"
+    export const placeholderQuestion = image.ofBuffer(hex``);
+    
+    //% fixedInstance jres blockIdentity=images._tile
+    //% tags="tile category-Placeholders"
+    export const placeholderBang = image.ofBuffer(hex``);
+    
+    //% fixedInstance jres blockIdentity=images._tile
+    //% tags="tile category-Placeholders"
+    export const placeholderRight = image.ofBuffer(hex``);
+    
+    //% fixedInstance jres blockIdentity=images._tile
+    //% tags="tile category-Placeholders"
+    export const placeholderDown = image.ofBuffer(hex``);
+    
+    //% fixedInstance jres blockIdentity=images._tile
+    //% tags="tile category-Placeholders"
+    export const placeholderLeft = image.ofBuffer(hex``);
+    
+    //% fixedInstance jres blockIdentity=images._tile
+    //% tags="tile category-Placeholders"
+    export const placeholderUp = image.ofBuffer(hex``);
+}
